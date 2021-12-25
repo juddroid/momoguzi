443e5516d285c53c3c01145dfb1d408b3ebe35c4
Raccoon <nobllizdc@gmail.com>
2021-12-26 07:08:58 +0900

    notion api

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,10 @@ import DATA from './data.json';
 import dotenv from 'dotenv';
 import { createLogger, transports } from 'winston';
 import { VALID_KEYWORD } from './const';
+import axios from 'axios';
 
 const app = express();
+dotenv.config();
 const logger = createLogger({
   transports: [
     new transports.Console(),
@@ -16,7 +18,7 @@ const logger = createLogger({
     new transports.File({ filename: './logs/combined.log' }),
   ],
 });
-dotenv.config();
+
 const PORT = process.env.PORT || 3000;
 const slackEvents = createEventAdapter(process.env.SIGNING_SECRET_TICTOC);
 const webClient = new WebClient(process.env.BOT_USER_OAUTH_ACCESS_TOKEN_TICTOC);
@@ -36,14 +38,17 @@ slackEvents.on('message', async (e) => {
 
     if (bot) return;
     if (keyword) {
-      const list = getListFromJSON(DATA.lunch);
-      const idx = getRandomNumber(list);
-      const { store, path } = list[idx];
+      // const list = getListFromJSON(DATA.lunch);
+      // const idx = getRandomNumber(list);
+      // const { store, path } = list[idx];
 
-      webClient.chat.postMessage({
-        text: `오늘은 ${store} 어때요?\r${path}`,
-        channel: channel,
-      });
+      // webClient.chat.postMessage({
+      //   text: `오늘은 ${store} 어때요?\r${path}`,
+      //   channel: channel,
+      // });
+
+      getNotionData(keyword, channel);
+
       return;
     }
     if (!keyword) {
@@ -58,6 +63,42 @@ slackEvents.on('message', async (e) => {
     logger.error(error.message);
   }
 });
+
+const getNotionData = (keyword, channel) => {
+  axios({
+    method: 'POST',
+    url: 'https://api.notion.com/v1/databases/f1ee04673b2a424aadd87cd4f3b9c014/query',
+    headers: {
+      Authorization: `Bearer ${process.env.NOTION_TOKEN}`,
+      'Notion-Version': `2021-08-16`,
+    },
+    data: {
+      filter: {
+        property: 'Name',
+        text: {
+          contains: '',
+        },
+      },
+    },
+  }).then((res) => {
+    const dataList = res.data.results
+      .map(({ properties }) => {
+        if (!properties.Name.title.length) return null;
+        return {
+          store: properties.Name.title[0].plain_text,
+          path: properties.path.rich_text[0].href,
+        };
+      })
+      .filter((data) => data);
+    console.log(dataList);
+    const idx = getRandomNumber(dataList);
+    const { store, path } = dataList[idx];
+    webClient.chat.postMessage({
+      text: `오늘은 ${store} 어때요?\r${path}\ridx: ${idx}\rkeyword:${keyword}`,
+      channel: channel,
+    });
+  });
+};
 
 // app.post('/slack/events', (req, res) => {
 //   console.log(req.body);
