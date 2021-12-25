ed3c79388de1ab021421bf7d5548f855f3ecdb62
Raccoon <nobllizdc@gmail.com>
2021-12-26 04:05:54 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -64,10 +64,10 @@ slackEvents.on('message', async (e) => {
 //   res.json(req.body);
 // });
 
-const webhook = () => console.log();
-
 app.use('/slack/events', slackEvents.requestListener());
-app.use('/github/payload', webhook);
+app.post('/github/payload', (req, res) => {
+  console.log(req.body);
+});
 
 createServer(app).listen(PORT, () => {
   console.log(`momoguzi starts on ${PORT}`);
