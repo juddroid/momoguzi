ac40d737267a21d51a138228d793d7ff7d7af21d
Raccoon <nobllizdc@gmail.com>
2022-01-09 21:54:56 +0900

    add path

--- src/index.ts
+++ src/index.ts
@@ -84,7 +84,7 @@ const getNotionData = (text, channel) => {
     const idx = getRandomNumber(dataList);
     const { store, path } = dataList[idx];
     webClient.chat.postMessage({
-      text: `오늘은 ${store} 어때요?\rpath:${'path'}`,
+      text: `오늘은 ${store} 어때요?\r${path}`,
       channel: channel,
       mrkdwn: true,
     });
