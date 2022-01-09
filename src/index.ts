d6bc02af28484376be87dcc231e48872c28bec2f
Raccoon <nobllizdc@gmail.com>
2022-01-09 22:14:07 +0900

    add path

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ slackEvents.on('message', async (e) => {
   console.log('');
   try {
     const { text, channel, bot_id, subtype } = e;
-    logger.info(e);
+    logger.info(e.message.text);
     if (subtype === 'message_changed') return console.log('===== subtype =====');
     if (!text) return console.log('=== !text ===');
 
