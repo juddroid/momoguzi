6cc44958b33abe507828a05352bcf1785ea4eba2
Raccoon <nobllizdc@gmail.com>
2022-05-07 21:40:06 +0900

    conflict

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,11 @@ import { VALID_KEYWORD } from './const';
 import axios from 'axios';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 dotenv.config();
+
 const logger = createLogger({
   transports: [
     new transports.Console(),
